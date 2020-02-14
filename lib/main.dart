b969796d55f5fafac5d568a0328525214adc2b15
Borba <gabriel.lewis@acad.pucrs.br>
2020-02-14 01:00:51 -0300

    Mapping Lists to Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,58 @@
 import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello'),);
+    var questions = [
+      {
+        'questionText': 'Whats your favorite animal?',
+        'answers': ['Shark', 'Dog', 'Cat', 'Wolf'],
+      },
+      {
+        'questionText': 'Whats your favorite color?',
+        'answers': ['Black', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'Whats your dog?',
+        'answers': ['sausage', 'golden', 'pug', 'labrador'],
+      },
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[questionIndex]['questionText'],
+            ),
+            ...(questions[questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(answerQuestion, answer);
+            }).toList()
+          ],
+        ),
+      ),
+    );
   }
 }
